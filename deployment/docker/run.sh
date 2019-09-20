fd17f31775d70f32d7314ab8510791d0de0e6ad0
ulfhaga <ulf.haga@gmail.com>
2019-09-20 19:29:27 +0200

    env host

--- deployment/docker/run.sh
+++ deployment/docker/run.sh
@@ -3,4 +3,4 @@ set -e
 declare CDIR=`dirname $0`
 cd ${CDIR}
 host_ip=$(hostname -I | cut -d" " -f1)
-docker run --env DOCKERHOST="$(hostname)" -v pool:/var/www/html/debtoox/pool --rm -dit --name debian-repository -p 58763:80 debian-repository:latest
+docker run -v pool:/var/www/html/debtoox/pool --rm -dit --name debian-repository -p 58763:80 debian-repository:latest
